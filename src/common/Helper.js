43b9712ba61817b2ffcd4a12b4967e4a9c3dd465
pathran-phantom1823 <patrick.pathran23@gmail.com>
2021-01-19 20:30:19 -0800

    setup

--- src/common/Helper.js
+++ src/common/Helper.js
@@ -28,7 +28,7 @@ import LogoutIcon from 'assets/drawer/logout_icon.svg';
 export default {
   company: 'Increment Technologies',
   APP_NAME: '@Agicord_',
-  APP_NAME_BASIC: 'Agicord',
+  APP_NAME_BASIC: 'SIML',
   APP_EMAIL: 'support@traceag.com.au',
   APP_WEBSITE: 'support@traceag.com.au',
   APP_HOST: 'com.agricord',
