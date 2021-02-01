a73c635d82c72b700da0af23e09bd088b9d0933a
pathran-phantom1823 <patrick.pathran23@gmail.com>
2021-02-01 08:01:44 +0800

    initial

--- src/modules/swiper/index.js
+++ src/modules/swiper/index.js
@@ -8,9 +8,9 @@ const ScreenWidth = Dimensions.get('window').width
 
 const Images = [
     { id: "1", uri: require('assets/logo_white.png') },
-    { id: "2", uri: require('assets/logo.png') },
+    { id: "2", uri: require('assets/logo_white.png') },
     { id: "3", uri: require('assets/logo_white.png') },
-    { id: "4", uri: require('assets/logo.png') },
+    { id: "4", uri: require('assets/logo_white.png') },
     { id: "5", uri: require('assets/logo_white.png') },
 ]
 
@@ -20,7 +20,11 @@ class Swiper extends Component{
 
         this.position = new Animated.ValueXY()
         this.state = {
-            currentIndex: 0
+            currentIndex: 0,
+            isMoveLeft: false,
+            isMoveRight: false,
+            scroll: true,
+            pan: new Animated.ValueXY(),
         }
 
         this.rotate = this.position.x.interpolate({
@@ -60,19 +64,33 @@ class Swiper extends Component{
             outputRange: [1, 0.8, 1],
             extrapolate: 'clamp'
         })
+        
     }
-    UNSAFE_componentWillMount(){
+    componentWillMount(){
         this.PanResponder = PanResponder.create({
             onStartShouldSetPanResponder: (evt, gestureState) => true,
+            onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+            onMoveShouldSetPanResponder: (evt, gestureState) => true,
+            onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+            onPanResponderGrant: () => this.setState({ scroll: true }),
             onPanResponderMove: (evt, gestureState) => {
-                if(gestureState.vx >= 0  && gestureState.vy >= 0){
-                    console.log('>>>>>>>>>>>>>>');
+                this.position.setValue({x: gestureState.dx, y: 0})
+                if(gestureState.dx <= 0){
+                  this.setState({isMoveLeft: true})
+                  this.setState({isMoveRight: false})
                 }else{
-                    console.log('<<<<<<<<<<<<<<<<<<<<<<<');
+                  this.setState({isMoveRight: true})
+                  this.setState({isMoveLeft: false})
                 }
-                this.position.setValue({x: gestureState.dx, y: gestureState.dy})
+                Animated.event([
+                  null,
+                  {dx: this.state.pan.x, dy:this.state.pan.y}
+                ])
+
             },
             onPanResponderRelease: (evt, gestureState) => {
+              this.setState({isMoveRight: false})
+              this.setState({isMoveLeft: false})
                 if(gestureState.dx > 120){
                     Animated.spring(this.position, {
                         toValue: { x: ScreenWidth + 100, y: gestureState.dy }
@@ -101,26 +119,22 @@ class Swiper extends Component{
         })
     }
     renderUsers = () => {
-
         return Images.map((item, i) => {
-    
-    
           if (i < this.state.currentIndex) {
             return null
           }
           else if (i == this.state.currentIndex) {
-    
             return (
               <Animated.View
                 {...this.PanResponder.panHandlers}
                 key={item.id} style={[this.rotateAndTranslate, { height: ScreenHeight - 120, width: ScreenWidth, padding: 10, position: 'absolute' }]}>
                 <Animated.View style={{ opacity: this.likeOpacity, transform: [{ rotate: '-30deg' }], position: 'absolute', top: 50, left: 40, zIndex: 1000 }}>
-                  <Text style={{ borderWidth: 1, borderColor: 'green', color: 'green', fontSize: 32, fontWeight: '800', padding: 10 }}>LIKE</Text>
+                  <Text style={{ borderWidth: 1, borderColor: this.state.isMoveRight ? 'green' : 'transparent', color: 'green', fontSize: 32, fontWeight: '800', padding: 10 }}>{this.state.isMoveRight ? 'LIKE' : null}</Text>
     
                 </Animated.View>
     
                 <Animated.View style={{ opacity: this.dislikeOpacity, transform: [{ rotate: '30deg' }], position: 'absolute', top: 50, right: 40, zIndex: 1000 }}>
-                  <Text style={{ borderWidth: 1, borderColor: 'red', color: 'red', fontSize: 32, fontWeight: '800', padding: 10 }}>NOPE</Text>
+                  <Text style={{ borderWidth: 1, borderColor: this.state.isMoveLeft ? 'red' : 'transparent', color: 'red', fontSize: 32, fontWeight: '800', padding: 10 }}>{this.state.isMoveLeft ? 'NOPE' : null}</Text>
     
                 </Animated.View>
     
@@ -134,7 +148,6 @@ class Swiper extends Component{
           else {
             return (
               <Animated.View
-    
                 key={item.id} style={[{
                   opacity: this.nextCardOpacity,
                   transform: [{ scale: this.nextCardScale }],
@@ -167,7 +180,7 @@ class Swiper extends Component{
     
             </View>
             <View style={{ flex: 1 }}>
-              {this.renderUsers()}
+              {this.state.currentIndex <= Images.length - 1 ? this.renderUsers() : this.renderUsers()}
             </View>
             <View style={{ height: 60 }}>
     
