77863cf298105477974290816abe66acf18e5327
kennck <kennettecanales@gmail.com>
2021-02-15 20:24:36 +0800

    udpate

---
+++ src/modules/generic/GroupUsers.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { View, TouchableOpacity, Image, Dimensions, Text } from 'react-native'
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faCheck, faTimes, faStar} from '@fortawesome/free-solid-svg-icons';
+import { BasicStyles, Color } from 'common';
+const height = Math.round(Dimensions.get('window').height);
+import UserImage from 'components/User/Image';
+
+class GroupUsers extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    const { data } = this.props;
+    return (
+			<View style={{
+        width: '100%',
+        position: 'relative',
+        flexDirection: 'row',
+        ...this.props.style
+      }}>
+        {
+          data && data.map((item, index) => (
+            <UserImage
+              user={item}
+              color={Color.white}
+              size={30}
+              style={{
+                width: 30,
+                height: 30,
+                borderRadius: 15
+              }}/>
+          ))
+        }
+			</View>
+    )
+  }
+}
+
+export default GroupUsers;
--- src/modules/history/Style.js
+++ src/modules/history/Style.js
@@ -1,6 +1,7 @@
 import { Color } from 'common';
 import { Dimensions } from 'react-native';
 const width = Math.round(Dimensions.get('window').width)
+const height = Math.round(Dimensions.get('window').height)
 export default {
   ScrollView: {
     flex: 1
@@ -12,5 +13,10 @@ export default {
   },
   footerIcon: {
     marginTop: Platform.OS == 'ios' ? 30 : 0
-  }
+  },
+  sliderContainer: {
+    width: '90%',
+    marginLeft: '5%',
+    marginRight: '5%'
+  },
 }
